609d350373601623b682dbab5f294b304f49e652
Mohd Rejoan <wearecrx@gmail.com>
2021-03-30 22:37:44 +0600

    project complete

---
+++ js/popUp.js
@@ -0,0 +1,8 @@
+const createPopup = document.createElement("div");
+createPopup.classList.add(".popup");
+// ----------------------------------------
+// load popup
+// -----------------------------------------
+
+const getImg = document.querySelectorAll(".image_highlight");
+console.log(getImg);
